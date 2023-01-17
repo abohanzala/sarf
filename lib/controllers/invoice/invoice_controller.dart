c2511143d1e449f6b0713d9840dee01959ec99fb
junaidshahid123 <junaid459561@gmail.com>
2023-01-17 13:24:35 +0500

    new updates

--- lib/controllers/invoice/invoice_controller.dart
+++ lib/controllers/invoice/invoice_controller.dart
@@ -12,39 +12,36 @@ import '../../constant/api_links.dart';
 import '../../services/dio_client.dart';
 import '../../src/widgets/loader.dart';
 
-class InvoiceController extends getpackage.GetxController{
+class InvoiceController extends getpackage.GetxController {
+  List<File> uploadImages = <File>[].obs;
 
- List<File> uploadImages = <File>[].obs;
- 
-  Future postNewInvoice(String mobile,String amount,String note) async {
-  openLoader();
-    
-   
+  Future postNewInvoice(String mobile, String amount, String note) async {
+    openLoader();
+
     FormData formData = FormData();
-    
-for (var i = 0; i < uploadImages.length; i++) {
-  var file = uploadImages[i];
-  String fileName = file.path.split('/').last;
-  formData.files.add(
-    MapEntry("file_attach[$i]", await MultipartFile.fromFile(file.path, filename: fileName))
-   );
-}
-  formData.fields.add(MapEntry('language', GetStorage().read('lang').toString()));
-  formData.fields.add(MapEntry('mobile', mobile));
-  formData.fields.add(MapEntry('amount', amount));
-  formData.fields.add(MapEntry('note', note));
-  formData.fields.add(const MapEntry('paid_status', "0"));
-  debugPrint(formData.fields.toString());
-  
-   //debugPrint(formData.files.first.toString());
-    
+
+    for (var i = 0; i < uploadImages.length; i++) {
+      var file = uploadImages[i];
+      String fileName = file.path.split('/').last;
+      formData.files.add(MapEntry("file_attach[$i]",
+          await MultipartFile.fromFile(file.path, filename: fileName)));
+    }
+    formData.fields
+        .add(MapEntry('language', GetStorage().read('lang').toString()));
+    formData.fields.add(MapEntry('mobile', mobile));
+    formData.fields.add(MapEntry('amount', amount));
+    formData.fields.add(MapEntry('note', note));
+    formData.fields.add(const MapEntry('paid_status', "0"));
+    debugPrint(formData.fields.toString());
+
+    //debugPrint(formData.files.first.toString());
+
     //Dio http = API.getInstance();
     var response = await DioClient()
-        .post(ApiLinks.simpleInvoice, formData,true)
+        .post(ApiLinks.simpleInvoice, formData, true)
         .catchError((error) {
-          getpackage.Get.back();
+      getpackage.Get.back();
       if (error is BadRequestException) {
-        
         var apiError = json.decode(error.message!);
         debugPrint("aaaaaaaa${error.toString()}");
         getpackage.Get.snackbar('Error', apiError["reason"].toString());
@@ -58,9 +55,8 @@ for (var i = 0; i < uploadImages.length; i++) {
     });
 
     // final response = await http.post(ApiLinks.addNewAdApi,data: formData );
-    
-     
-      debugPrint("aaaaaaaaaaa$response");
+
+    debugPrint("aaaaaaaaaaa$response");
     // Navigator.of(getpackage.Get.context!).pop();
     if (response == null) return;
     if (response['success']) {
@@ -70,57 +66,52 @@ for (var i = 0; i < uploadImages.length; i++) {
       uploadImages.clear();
       getpackage.Get.back();
       //files.clear();
-      
-      
+
     } else {
       uploadImages.clear();
       debugPrint('here');
       (response.containsKey('validation_errors'))
-          ? getpackage.Get.snackbar(response['message'].toString(), response['validation_errors'].toString())
+          ? getpackage.Get.snackbar(response['message'].toString(),
+              response['validation_errors'].toString())
           // SnakeBars.showValidationErrorSnake(
           //     title: response['message'].toString(),
           //     description: response['validation_errors'].toString())
           : getpackage.Get.snackbar('Success', response['message'].toString());
-          //SnakeBars.showErrorSnake(description: response['message'].toString());
+      //SnakeBars.showErrorSnake(description: response['message'].toString());
       Navigator.of(getpackage.Get.context!).pop();
     }
   }
 
-   Future<InvoiceList?> getInvoiceList() async {
+  Future<InvoiceList?> getInvoiceList() async {
     //print("${ApiLinks.membersList}${GetStorage().read('lang')}");
-   // openLoader();
-    var response =
-        await DioClient().get("${ApiLinks.invoiceList}${GetStorage().read('lang')}").catchError((error) {
+    // openLoader();
+    var response = await DioClient()
+        .get("${ApiLinks.invoiceList}${GetStorage().read('lang')}")
+        .catchError((error) {
       if (error is BadRequestException) {
-        
-        
-       // print(error.toString());
+        // print(error.toString());
       } else {
-
         //print(error.toString());
-        
-    if (error is BadRequestException) {
-     // print(error.toString());
-      
-    } else if (error is FetchDataException) {
-     // print(error.toString());
-      
-    } else if (error is ApiNotRespondingException) {
-      //print(error.message.toString());
-    }
 
+        if (error is BadRequestException) {
+          // print(error.toString());
+
+        } else if (error is FetchDataException) {
+          // print(error.toString());
+
+        } else if (error is ApiNotRespondingException) {
+          //print(error.message.toString());
+        }
       }
-    }); 
+    });
     if (response['success'] == true) {
-     
       debugPrint(response.toString());
-        var membersList = InvoiceList.fromJson(response);
-        //print(membersList.data?.first.expenseName);
-        return membersList;
+      var membersList = InvoiceList.fromJson(response);
+      //print(membersList.data?.first.expenseName);
+      return membersList;
     } else {
       debugPrint('here');
     }
     return null;
   }
-
 }
